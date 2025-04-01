8ff684215675a33bc28669d10036fffd8f08549b
Matias Casiba <casibagabriel@gmail.com>
2025-04-01 13:40:17 -0300

    Anulé el contenedor para tema oscuro dentro de SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
                 <div className="search-bar__logo-container">
                     <img className="search-bar__logo-img" src="/logo/ds-logo-sf.png" alt="logo ds" />
                 </div>
-                <button className="theme-toggle">⚫</button>
+                {/* <button className="theme-toggle">⚫</button> */}
                 <form action="#" className="search-bar__form-container">
                     <label htmlFor="busqueda" className="search-bar__form-label">
                         <img className="search-bar__logo-search" src="/logo/logo-search.png" alt="logo del bucador" />
